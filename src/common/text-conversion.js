92f7de3617414121c5d3a334e3c732f64658dd2b
Michał Zięba <ziebik@gmail.com>
2015-06-07 23:48:37 +0200

    new methods: binToStr and strToBin

--- src/common/text-conversion.js
+++ src/common/text-conversion.js
@@ -1,8 +1,10 @@
 exports.hexToStr = function (hexString) {
     var result = [];
+
     hexString.match(/.{2}/g).forEach(function (pair) {
         result.push(String.fromCharCode(parseInt(pair, 16)));
     });
+
     return result.join('');
 };
 
@@ -14,6 +16,28 @@ exports.strToHex = function (string) {
             ('0' + string.charCodeAt(i).toString(16)).substr(-2, 2)
         );
     }
+
+    return result.join('');
+};
+
+exports.strToBin = function (string) {
+    var result = [];
+
+    for (var i = 0; i < string.length; ++i) {
+        result.push(
+            ('00000000' + string.charCodeAt(i).toString(2)).substr(-8, 8)
+        );
+    }
+
+    return result.join('');
+};
+
+exports.binToStr = function (binary) {
+    var result = [];
+
+    binary.match(/.{8}/g).forEach(function (byte) {
+        result.push(String.fromCharCode(parseInt(byte, 2)));
+    });
 
     return result.join('');
 };
