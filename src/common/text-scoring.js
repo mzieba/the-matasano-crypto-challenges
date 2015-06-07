4620af42c69574acb2f428a5c6e03d8941bb6cf3
Michał Zięba <ziebik@gmail.com>
2015-06-08 00:03:57 +0200

    method for calculating the Hamming distance

--- src/common/text-scoring.js
+++ src/common/text-scoring.js
@@ -24,3 +24,26 @@ exports.simple = function (textToScore, table) {
     return score;
 };
 
+exports.hammingDistance = function (a, b) {
+    if (a.length != b.length) {
+        return false;
+    }
+
+    var result = 0,
+        aBin = '',
+        bBin = '';
+
+    for (var i = 0; i < a.length; ++i) {
+        aBin = ('00000000' + a.charCodeAt(i).toString(2)).substr(-8, 8);
+        bBin = ('00000000' + b.charCodeAt(i).toString(2)).substr(-8, 8);
+
+        for (var j = 0; j < 16; ++j) {
+            if (aBin.charAt(j) != bBin.charAt(j)) {
+                ++result;
+            }
+        }
+    }
+
+    return result;
+};
+
